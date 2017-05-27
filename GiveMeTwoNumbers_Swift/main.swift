62c38e7a3396780153eef025feb5ff7d03676d4f
leonguevara <leon.guevara@outlook.com>
2017-05-27 13:13:49 -0500

    Initial Commit

---
+++ GiveMeTwoNumbers_Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GiveMeTwoNumbers_Swift
+//
+//  Created by León Felipe Guevara Chávez on 5/27/17.
+//  Copyright © 2017 León Felipe Guevara Chávez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
